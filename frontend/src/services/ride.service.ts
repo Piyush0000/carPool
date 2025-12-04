b61c85a3d9909ed9ac9ad26e0bd3e64ba884c6ad
Piyush <rathorepiyush0000@gmail.com>
2025-12-05 03:06:38 +0530

    Fixedddddd

--- frontend/src/services/ride.service.ts
+++ frontend/src/services/ride.service.ts
@@ -213,8 +213,7 @@ class RideService {
    * @param upiId - The UPI ID
    * @returns Promise with updated ride
    */
-  static async updateDriverUpiId(rideId: string, upiId: string): Promise<Ride> {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static async updateDriverUpiId(_rideId: string, _upiId: string): Promise<Ride> {
     throw new Error('UPI ID functionality has been removed. Please use QR code only.');
   }
 
